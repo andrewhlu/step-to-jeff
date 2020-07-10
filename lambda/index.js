04df2d10f155e175039d9a73e8f58f1314724628
Andrew Lu <alu@ucsb.edu>
2020-07-10 01:49:54 -0700

    Set up Actions workflow to deploy skill to Alexa

--- lambda/index.js
+++ lambda/index.js
@@ -21,7 +21,7 @@ const HelloWorldIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'uwu owo!';
+        const speakOutput = 'uwu, owo, nuzzle wuzzle blush!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')

916dbbacc43732969ed6a3c6b2e54125aeef8d05
Andrew Lu <alu@ucsb.edu>
2020-07-30 01:51:08 -0700

    Completed Gmail account search

--- lambda/gmail.js
+++ lambda/gmail.js
@@ -20,7 +20,7 @@ const searchTerms = [
         ]
     },
     {
-        title: "Offer Accepted",
+        title: "Offer Accepted and Survey",
         query: "from:(noreply@qemailserver.com) subject:(Congrats from Amazon + Onboarding Logistics) We are excited to see you have accepted your offer!",
         parse: [
             {
@@ -31,14 +31,32 @@ const searchTerms = [
             },
             {
                 key: "link",
-                matcher: "Survey LInk:",
+                matcher: "https://amazonuni.qualtrics.com/jfe/form",
                 delimiter: "Survey ID:",
-                afterMatcherText: true
+                afterMatcherText: false
             },
             {
                 key: "id",
                 matcher: "Survey ID: ",
                 delimiter: "\n",
+                afterMatcherText: true
+            }
+        ]
+    },
+    {
+        title: "Offer Survey Confirmation",
+        query: "from:(noreply@qemailserver.com) subject:(Welcome to Amazon - Survey Confirmation) This is a confirmation that your responses have been received.",
+        parse: [
+            {
+                key: "title",
+                matcher: "below to see which d...",
+                delimiter: "Are you completing",
+                afterMatcherText: true,
+            },
+            {
+                key: "startDate",
+                matcher: "CPT, please check...",
+                delimiter: "-",
                 afterMatcherText: true
             }
         ]
@@ -78,12 +96,158 @@ const searchTerms = [
                 afterMatcherText: true
             }
         ]
+    },
+    {
+        title: "Information Session Confirmation",
+        query: "from:(customercare@gotowebinar.com) subject:(Confirming Registration: Amazon Internship Q&A) Thank you for registering for Amazon Internship Q&A",
+        parse: [
+            {
+                key: "date",
+                matcher: "             ",
+                delimiter: "1. Click the link",
+                afterMatcherText: true
+            },
+            {
+                key: "link",
+                matcher: "https://global.gotowebinar.com",
+                delimiter: "https://link",
+                afterMatcherText: false
+            }
+        ]
+    },
+    {
+        title: "Relocation",
+        query: "from:graebel.com subject:(Welcome to Graebel globalCONNECT!) globalCONNECT Password",
+        parse: [
+            {
+                key: "link",
+                matcher: "https://myrelocation.graebel.com",
+                delimiter: "\">",
+                afterMatcherText: false
+            }
+        ]
+    },
+    {
+        title: "Organization Confirmation",
+        query: "from:(asp-offersonboarding@amazon.com) subject:(Amazon organization confirmation) Congratulations on your offer and decision to join Amazon!",
+        parse: [
+            {
+                key: "organization",
+                matcher: "in the ",
+                delimiter: " organization",
+                afterMatcherText: true
+            }    
+        ]
+    },
+    {
+        title: "Manager Intro",
+        query: "from:(asp-offersonboarding@amazon.com) subject:(Welcome to Amazon - Manager Intro) introduce you to your manager",
+        parse: [
+            {
+                key: "name",
+                matcher: "your manager, ",
+                delimiter: ", who is",
+                afterMatcherText: true
+            }
+        ]
+    },
+    {
+        title: "AWS Educate",
+        query: "from:(notifications@instructure.com) subject:(Course Invitation) AWS Academy Cloud Foundations",
+        parse: [
+            {
+                key: "link",
+                matcher: "https://awsacademy.instructure.com",
+                delimiter: "________________________________________",
+                afterMatcherText: false
+            }
+        ]
+    },
+    {
+        title: "New Hire Documents",
+        query: "from:(MyDocs-noreply@onbaseonline.com) subject:(You Have Documents to Review and Electronically Sign) Your new hire employment documents",
+        parse: [
+            {
+                key: "link",
+                matcher: "https://amazon.onbaseonline.com",
+                delimiter: "<",
+                afterMatcherText: false
+            }    
+        ]
+    },
+    {
+        title: "Embark",
+        query: "from:(newhiresupport@amazon.com) subject:(Welcome Amazonian! Start exploring Amazon’s peculiar ways) Amazon Embark",
+        parse: [
+            {
+                key: "link",
+                matcher: "Visit Amazon Embark<",
+                delimiter: ">",
+                afterMatcherText: true
+            }
+        ]
+    },
+    {
+        title: "Equipment Delivery",
+        query: "from:(noreply@qemailserver.com) subject:(Mailing address for Amazon Equipment Delivery) ship your equipment",
+        parse: [
+            {
+                key: "dueDate",
+                matcher: "no later than ",
+                delimiter: ".",
+                afterMatcherText: true
+            },
+            {
+                key: "link",
+                matcher: "https://amazonuni.qualtrics.com/jfe/form",
+                delimiter: ">",
+                afterMatcherText: false
+            },
+        ]
+    },
+    {
+        title: "I-9 Verification Section 1",
+        query: "from:(amazon-i9@i9advantage.com) subject:(Remote Hire Employment Eligibility) Form I-9",
+        parse: [
+            {
+                key: "link",
+                matcher: "https://hr.i9advantage.com",
+                delimiter: "Enter the",
+                afterMatcherText: false
+            },
+            {
+                key: "pin",
+                matcher: "PIN:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;",
+                delimiter: "Please contact",
+                afterMatcherText: true
+            }
+        ]
+    },
+    {
+        title: "I-9 Verification Section 1 Completion",
+        query: "from:(amazon-i9@i9advantage.com) subject:(Remote Hire Section 1 Completion) Form I-9"
+    },
+    {
+        title: "New Hire Orientation Info",
+        query: "from:(newhiresupport@amazon.com) subject:(Welcome to Amazon! New Hire Orientation Info. Enclosed) New Hire Orientation"
+    },
+    {
+        title: "Midway Temporary PIN",
+        query: "from:(registration-pin-no-reply@amazon.com) subject:(Here's your temporary PIN) temporary PIN",
+        parse: [
+            {
+                key: "pin",
+                matcher: "Your temporary PIN: ",
+                delimiter: "</p>",
+                afterMatcherText: true
+            }    
+        ]
     }
 ];
 
 function parseEmailText(text, matcher, delimiter, afterMatcherText) {
     let beginningIndex = text.indexOf(matcher)  + (afterMatcherText ? matcher.length : 0);
-    return text.indexOf(matcher) < 0 ? "" : text.substr(beginningIndex, text.substring(beginningIndex).indexOf(delimiter));
+    return text.indexOf(matcher) < 0 ? "" : text.substr(beginningIndex, text.substring(beginningIndex).indexOf(delimiter)).replace(/^\s+|\s+$/g, '');
 }
 
 module.exports.sayHi = (accessToken) => {
@@ -124,30 +288,28 @@ module.exports.searchForEmails = async (handlerInput) => {
                 console.log(item.title + " has " + res.data.resultSizeEstimate + " matching emails.");
                 
                 if(res.data.resultSizeEstimate > 0) {
-                    res.data.messages.forEach((message) => {
-                        // Get the message
-                        gmail.users.messages.get({
-                            id: message.id,
-                            userId: 'me',
-                        }, async (err, res) => {
-                            if(err) {
-                                throw err;
-                            }
-                            
-                            let payload = res.data.payload;
-                            while(payload.parts) {
-                                payload = payload.parts[0];
-                            }
-                            
-                            let text = Buffer(payload.body.data, 'base64').toString();
-                            // console.log("Decoded: " + text);
-                            
-                            if(item.parse) {
-                                item.parse.forEach((criteria) => {
-                                    console.log(item.title + " has key " + criteria.key + " with value: " + parseEmailText(text, criteria.matcher, criteria.delimiter, criteria.afterMatcherText));
-                                })
-                            }
-                        });
+                    // Get the message
+                    gmail.users.messages.get({
+                        id: res.data.messages[0].id,
+                        userId: 'me',
+                    }, async (err, res) => {
+                        if(err) {
+                            throw err;
+                        }
+                        
+                        let payload = res.data.payload;
+                        while(payload.parts) {
+                            payload = payload.parts[0];
+                        }
+                        
+                        let text = Buffer(payload.body.data, 'base64').toString();
+                        // console.log("Decoded: " + text);
+                        
+                        if(item.parse) {
+                            item.parse.forEach((criteria) => {
+                                console.log(item.title + " has key " + criteria.key + " with value: " + parseEmailText(text, criteria.matcher, criteria.delimiter, criteria.afterMatcherText));
+                            });
+                        }
                     });
                 }
             });
